6f4ee4c5f4febe8af9994247282db2be23447a32
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-23 16:54:41 +0400

    delete console logs, rename file

--- dayOfYear.js
+++ dayOfYear.js
@@ -28,11 +28,3 @@ function dayOfYear(date) {
   }
   return res + day
 }
-
-
-console.log(dayOfYear("12/13/2020"));
-console.log(dayOfYear("12/17/2020"));
-console.log(dayOfYear("11/16/2020"));
-console.log(dayOfYear("1/9/2019"));
-console.log(dayOfYear("3/1/2004"));
-console.log(dayOfYear("12/31/2000"));
