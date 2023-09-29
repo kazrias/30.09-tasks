6670703d81650490c5ef684ced79a97d96d5bfa4
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-29 21:00:27 +0400

    delete comment from  dayOfYear function

--- dayOfYear.js
+++ dayOfYear.js
@@ -1,5 +1,4 @@
 function dayOfYear(date) {
-  // let daysAmount = {
   let dt = new Date(date);
   let started = new Date(dt.getFullYear(), 0, 1)
   return (dt.getTime() - started.getTime()) / (1000 * 60 * 60 * 24) + 1
