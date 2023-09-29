25653a27edb911ae990c65f8215cbd7965d74f98
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-29 21:04:14 +0400

    delete console log

--- dayOfYear.js
+++ dayOfYear.js
@@ -3,4 +3,3 @@ function dayOfYear(date) {
   let started = new Date(dt.getFullYear(), 0, 1)
   return (dt.getTime() - started.getTime()) / (1000 * 60 * 60 * 24) + 1
 }
-console.log(dayOfYear("3/1/2004"));
