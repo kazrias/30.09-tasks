5049c06b3072c16d87caeee465eb66b00e6d5440
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-29 22:01:53 +0400

    getWeekOfMonth function add

--- getWeekOfMonth.js
+++ getWeekOfMonth.js
@@ -1,6 +1,10 @@
 function getWeekOfMonth(date) {
-  const dateStart = new Date(date.getFullYear(), date.getMonth(), 1)
-  return Math.ceil(date.getDate() / 7)
+  const monthStart = new Date(date.getFullYear(), date.getMonth(), 1);
+  const firstWeekDay = monthStart.getDay();
+  const monthDay = date.getDate();
+  return Math.ceil((firstWeekDay + monthDay) / 7);
 }
 
-console.log(getWeekOfMonth(new Date(2023,0,24)));
+console.log(getWeekOfMonth(new Date(2017, 10, 9)));
+console.log(getWeekOfMonth(new Date(2023, 0, 24)));
+console.log(getWeekOfMonth(new Date()));
