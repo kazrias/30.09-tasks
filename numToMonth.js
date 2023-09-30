6e971d21d5cc63305f7598f29fcff34c2cf113f1
Artyom Harutyunyan <ka6rias@gmail.com>
2023-09-30 13:39:03 +0400

    numToMonth function finish

---
+++ numToMonth.js
@@ -0,0 +1,4 @@
+function numToMonth(num) {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return months[num - 1]
+}
